9de49c3f29c608010dbb4e9b88c00777c8793e57
hedinang <tridunga1@gmail.com>
2023-12-26 01:49:13 +0700

    gl

--- src/pages/TestGL.js
+++ src/pages/TestGL.js
@@ -1,10 +1,13 @@
 /* eslint-disable import/first */
 import { useState } from 'react';
 import DeckGL from '@deck.gl/react';
+import { Card } from "antd"
 import { LineLayer, TextLayer, IconLayer } from '@deck.gl/layers';
+
 import { Map } from 'react-map-gl';
 import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
+// import "maplibre-gl/dist/maplibre-gl.css";
+import './map.scss'
 
 // Data to be used by the LineLayer
 // const data = [
@@ -13,41 +16,23 @@ import "maplibre-gl/dist/maplibre-gl.css";
 // ];
 
 const ICON_MAPPING = {
-  marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+  marker: { x: 0, y: 0, width: 128, height: 128, mask: true }
 };
 
-// const data = [
-//   { name: 'Colma (COLM)', address: '365 D Street, Colma CA 94014', coordinates: [-122.466233, 37.684638] },
-// ]
-
 const data = [
-  { name: 'Colma (COLM)', address: '365 D Street, Colma CA 94014', exits: 4214, coordinates: [-122.466233, 37.684638] },
+  { name: 'Colma (COLM)', address: '376 buoi', exits: 4214, coordinates: [105.81465008529332, 21.04087243204058] },
 ]
+
 export default function TestGL() {
   const [viewState, setViewState] = useState({
-    longitude: 0,
-    latitude: 0,
-    zoom: 1
+    longitude: 105.81465008529332,
+    latitude: 21.04087243204058,
+    zoom: 15
   });
-
-  // const layer = new TextLayer({
-  //   id: 'text-layer',
-  //   data,
-  //   pickable: true,
-  //   getPosition: d => d.coordinates,
-  //   getText: d => d.name,
-  //   getSize: 10,
-  //   getAngle: 0,
-  //   getTextAnchor: 'middle',
-  //   getAlignmentBaseline: 'center'
-  // });
-
   const layer = new IconLayer({
     id: 'icon-layer',
     data,
     pickable: true,
-    // iconAtlas and iconMapping are required
-    // getIcon: return a string
     iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
     iconMapping: ICON_MAPPING,
     getIcon: d => 'marker',
@@ -59,19 +44,22 @@ export default function TestGL() {
   });
 
   return (
-    <div>
-      <DeckGL
-        style={{ width: 600, height: 400, overflow: "hidden" }}
-        viewState={viewState}
-        onViewStateChange={({ viewState }) => setViewState(viewState)}
-        controller={true}
-        layers={[layer]}
-      >
-        <Map
-          mapLib={maplibregl}
-          mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-        />
-      </DeckGL>
+    <div className='map'>
+      <Card hoverable className='map-card'>
+        <DeckGL
+          viewState={viewState}
+          onViewStateChange={({ viewState }) => setViewState(viewState)}
+          // controller={{ dragPan: false }}
+          controller={true}
+          layers={[layer]}
+        >
+          <Map
+            mapLib={maplibregl}
+            mapStyle='https://api.maptiler.com/maps/topo/style.json?key=6XB2ErrAynZGeRhk684q'
+          />
+        </DeckGL>
+      </Card >
+
     </div>
   );
 }
